65c1655fe10e9f869d8b18b27e08f8f35b034ea2
Lekanswanson <A00238705@student.ait.ie>
2020-10-08 13:29:22 +0100

    main commit hopefully

--- agile-assignment-1/src/test/Test.java
+++ agile-assignment-1/src/test/Test.java
@@ -1,24 +1,10 @@
 package test;
 
-import java.util.Scanner;
+public class Test {
 
-public class Test
-{
-	public Test
+	public static void main(String[] args) 
 	{
-		Scanner kb = new Scanner(System.in)
-				
-		System.out.println("Enter a random number: ");
-		int num = kb.nextInt();
-		
-		System.out.println();
-		System.out.println("Your number is " + num);
-	
+
 	}
 
-}
-
-public static void main(String[] args) 
-{
-	Test mytest = new Test();
 }
