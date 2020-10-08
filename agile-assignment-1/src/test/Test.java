d5ffc182c298321bc35678b13dcd3ffaf216a4e1
Lekanswanson <A00238705@student.ait.ie>
2020-10-08 13:05:50 +0100

    Holla if any of you see this changes. DOne with eclipse

--- agile-assignment-1/src/test/Test.java
+++ agile-assignment-1/src/test/Test.java
@@ -1,9 +1,24 @@
 package test;
 
-public class Test {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-
+public class Test
+{
+	public Test
+	{
+		Scanner kb = new Scanner(System.in)
+				
+		System.out.println("Enter a random number: ");
+		int num = kb.nextInt();
+		
+		System.out.println();
+		System.out.println("Your number is " + num);
+	
 	}
 
+}
+
+public static void main(String[] args) 
+{
+	Test mytest = new Test();
 }
