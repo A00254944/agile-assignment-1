ef37a473be5a154d596283b476f5ec3362d32f7d
Lekanswanson <A00238705@student.ait.ie>
2020-10-13 01:10:48 +0100

    to main

---
+++ HelloWorld/src/helloworld/MyProduct.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class MyProduct 
+{
+
+	public MyProduct() 
+	{
+		System.out.println("Product is 20");
+	}
+
+	public static void main(String[] args) 
+	{
+		MyProduct newProduct = new MyProduct();
+	}
+
+}
