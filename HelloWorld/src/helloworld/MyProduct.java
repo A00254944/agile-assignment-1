eaa684cbe39a8cab6ad2e376b316445f46f5cffd
Lekanswanson <A00238705@student.ait.ie>
2020-10-13 01:20:50 +0100

    My solution

--- HelloWorld/src/helloworld/MyProduct.java
+++ HelloWorld/src/helloworld/MyProduct.java
@@ -1,12 +1,31 @@
 package helloworld;
 
+import java.util.Scanner;
+
 public class MyProduct 
 {
 
 	public MyProduct() 
 	{
-		System.out.println("Product is 20");
+		Scanner input = new Scanner(System.in);
+		
+		int count = 1;
+		int product = 1;
+		
+		do {
+			
+			System.out.print("Enter number " + count + ": ");
+			int number = input.nextInt();
+			
+			
+			System.out.println("Product is: " + (product *= number) + "\n");
+			count++;
+			
+			
+		}while(count < 4);
+		
 	}
+		
 
 	public static void main(String[] args) 
 	{
